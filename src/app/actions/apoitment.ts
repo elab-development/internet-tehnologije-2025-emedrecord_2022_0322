db35b1723cfd72f798c90f4b42cdb7f4615bad4f
ana <bgana.rs@gmail.com>
2026-02-06 22:11:23 +0100

    dodata funkcionalnost zakazivanja pregleda

--- src/app/actions/apoitment.ts
+++ src/app/actions/apoitment.ts
@@ -1,10 +1,42 @@
 "use server";
 
 import {db} from "@/lib/prisma";
+import { AppointmentSchema } from "@/lib/schema";
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { AppointmentStatus } from "@prisma/client";
 
+export async function createNewAppointment(data: any) {
+  try {
+    const validatedData = AppointmentSchema.safeParse(data);
+
+    if (!validatedData.success) {
+      return { success: false, msg: "Invalid data" };
+    }
+    const validated = validatedData.data;
+
+    await db.appointment.create({
+      data: {
+        patient_id: data.patient_id,
+        doctor_id: validated.doctor_id,
+        time: validated.time,
+        type: validated.type,
+        appointment_date: new Date(validated.appointment_date),
+        note: validated.note,
+      },
+    });
+
+    return {
+      success: true,
+      message: "Appointment booked successfully",
+    };
+  } catch (error) {
+    console.log(error);
+    return { success: false, msg: "Internal Server Error" };
+  }
+}
 export async function appointmentAction(
   id: string | number,
+
   status: AppointmentStatus,
   reason: string
 ) {
@@ -27,3 +59,4 @@ export async function appointmentAction(
     return { success: false, msg: "Internal Server Error" };
   }
 }
+
